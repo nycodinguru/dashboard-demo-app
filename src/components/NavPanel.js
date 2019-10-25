4d5c0d57aea5277a08475fc5596213089f0b39cf
Rashad Rose <rashadd.rose@gmail.com>
2019-10-25 13:51:36 -0400

    formatted code

--- src/components/NavPanel.js
+++ src/components/NavPanel.js
@@ -6,9 +6,9 @@ export default function NavPanel() {
     const _floatingNavButton = React.createRef();
 
     useEffect(() => {
-        if (window.innerWidth > 766){
-            setTimeout( () => openNav(), 1000)
-            setTimeout( () => closeNav(), 2000)
+        if (window.innerWidth > 766) {
+            setTimeout(() => openNav(), 1000)
+            setTimeout(() => closeNav(), 2000)
         }
         return () => {
             closeNav()
@@ -16,23 +16,23 @@ export default function NavPanel() {
         };
     })
 
-    function openNavHelper(){
+    function openNavHelper() {
         if (window.innerWidth > 766) openNav()
     }
 
-    function closeNavHelper(){
+    function closeNavHelper() {
         if (window.innerWidth > 766) closeNav()
     }
 
-    function openNav(){
+    function openNav() {
         _navPanel.current.classList.remove('Closed')
         document.querySelector('.Dashboard-container').classList.add('Open')
         document.querySelector('body').classList.add('Open')
         _floatingNavButton.current.classList.add('Open')
     }
 
-    function closeNav(){
-        setTimeout( () => {
+    function closeNav() {
+        setTimeout(() => {
             _navPanel.current.classList.add('Closed')
             document.querySelector('.Dashboard-container').classList.remove('Open')
             document.querySelector('body').classList.remove('Open')
@@ -41,30 +41,30 @@ export default function NavPanel() {
     }
 
 
-    const navMenu = navMenuItems.map( (i, key) => {
-            return (
+    const navMenu = navMenuItems.map((i, key) => {
+        return (
             <ul className={`${i.group}`} key={key + 1}>
-                {i.names.map( (g, key) => { return  <li className={`Nav-menu-item ${g.name}`} key={key + 2} onClick={() => closeNav()}></li>})}
+                {i.names.map((g, key) => { return <li className={`Nav-menu-item ${g.name}`} key={key + 2} onClick={() => closeNav()}></li> })}
             </ul>
-            )
-        } )
-    
+        )
+    })
+
 
     return (
         <React.Fragment>
-        <div className='Nav-panel-container Closed' ref={_navPanel} onMouseEnter={() => openNavHelper()} onMouseLeave={() => closeNavHelper()}>
-            <div className='Close-button-parent'>
-                <div className='Close-button' onClick={() => closeNav()}></div>  
+            <div className='Nav-panel-container Closed' ref={_navPanel} onMouseEnter={() => openNavHelper()} onMouseLeave={() => closeNavHelper()}>
+                <div className='Close-button-parent'>
+                    <div className='Close-button' onClick={() => closeNav()}></div>
+                </div>
+                <h3 className='Nav-panel-logo'>Linus</h3>
+                <div className='Nav-menus'>
+                    {navMenu}
+                </div>
             </div>
-            <h3 className='Nav-panel-logo'>Linus</h3>
-            <div className='Nav-menus'>
-                {navMenu}
-            </div>
-        </div>
-        <div className='Floating-nav-button' ref={_floatingNavButton} onClick={() => openNav()}>
+            <div className='Floating-nav-button' ref={_floatingNavButton} onClick={() => openNav()}>
                 <div className='Floating-nav-button-label'>Menu</div>
                 <div className='Floating-nav-button-icon'></div>
-        </div>
+            </div>
         </React.Fragment>
-        )
-    }
+    )
+}
--- src/components/Settings/SettingsPanels.js
+++ src/components/Settings/SettingsPanels.js
@@ -11,95 +11,95 @@ const SettingsPanels = () => {
         _profilePrivacy: '',
         _searchPrivacy: '',
         navigationTheme: ''
-    }); 
+    });
 
     const handleToggle = (e) => {
         const name = e.target.getAttribute('name');
 
-        if (e.target.classList.contains('off')){
+        if (e.target.classList.contains('off')) {
             setToggles({
-                ...toggles, 
+                ...toggles,
                 [name]: 'on'
             })
-            document.querySelectorAll(`.${name}`).forEach( (k) => {
+            document.querySelectorAll(`.${name}`).forEach((k) => {
                 k.classList.remove('off')
                 k.classList.add('on')
-            })    
-        } 
-        else if (e.target.classList.contains('on')){
+            })
+        }
+        else if (e.target.classList.contains('on')) {
             setToggles({
-                ...toggles, 
+                ...toggles,
                 [name]: 'off'
             })
-            document.querySelectorAll(`.${name}`).forEach( (k) => {
+            document.querySelectorAll(`.${name}`).forEach((k) => {
                 k.classList.remove('on')
                 k.classList.add('off')
             })
         }
     }
 
-    const panels = settings.map( (i, key) => {
+    const panels = settings.map((i, key) => {
         return (
             <div className='Settings-panel' key={key + 1}>
                 <h3 className='Settings-title'>{i.title}</h3>
-                    { i.settings.map( (j, key) => {
-                        return (
-                            <div className='Setting' key={key + 2}>
-                                <div className='Setting-left'>
-                                    <h4 className={`Setting-name ${ j.info ? 'info' : '' }`}>{j.name}</h4>
-                                    <p className='Setting-description'>{j.description}</p>
-                                </div>
-                                <div className='Setting-right'>
-                                    {j.control === 'dropdown' ? 
-                                                                <div className='Select-wrapper'>
-                                                                    <select className='Select-drop-down' name=''>
-                                                                    { j.values.map( (o, key) => { 
-                                                                            return (
-                                                                                <option value={`${o.toLowerCase()}`} key={key + 3}>
-                                                                                    {o.toUpperCase()}
-                                                                                </option> 
-                                                                            )
-                                                                        })
-                                                                    }
-                                                                    </select>
-                                                                </div> : j.control === 'toggle' ?
-                                                                                                <div 
-                                                                                                    className={`Toggle ${j.values[0]} ${j.toggleName}`}
-                                                                                                    name={`${j.toggleName}`} >
-                                                                                                    <div 
-                                                                                                        className={`Toggle-switch ${j.values[0]} ${j.toggleName}`} 
-                                                                                                        name={`${j.toggleName}`}>
-                                                                                                        <div 
-                                                                                                            className={`Toggle-cover ${j.values[0]} ${j.toggleName}`} 
-                                                                                                            name={`${j.toggleName}`}  
-                                                                                                            onClick={ (e) => handleToggle(e)}>
-                                                                                                        </div>
-                                                                                                    </div>
-                                                                                                </div> : ''}
-                                </div>   
+                {i.settings.map((j, key) => {
+                    return (
+                        <div className='Setting' key={key + 2}>
+                            <div className='Setting-left'>
+                                <h4 className={`Setting-name ${j.info ? 'info' : ''}`}>{j.name}</h4>
+                                <p className='Setting-description'>{j.description}</p>
                             </div>
-                    ) 
+                            <div className='Setting-right'>
+                                {j.control === 'dropdown' ?
+                                    <div className='Select-wrapper'>
+                                        <select className='Select-drop-down' name=''>
+                                            {j.values.map((o, key) => {
+                                                return (
+                                                    <option value={`${o.toLowerCase()}`} key={key + 3}>
+                                                        {o.toUpperCase()}
+                                                    </option>
+                                                )
+                                            })
+                                            }
+                                        </select>
+                                    </div> : j.control === 'toggle' ?
+                                        <div
+                                            className={`Toggle ${j.values[0]} ${j.toggleName}`}
+                                            name={`${j.toggleName}`} >
+                                            <div
+                                                className={`Toggle-switch ${j.values[0]} ${j.toggleName}`}
+                                                name={`${j.toggleName}`}>
+                                                <div
+                                                    className={`Toggle-cover ${j.values[0]} ${j.toggleName}`}
+                                                    name={`${j.toggleName}`}
+                                                    onClick={(e) => handleToggle(e)}>
+                                                </div>
+                                            </div>
+                                        </div> : ''}
+                            </div>
+                        </div>
+                    )
                 })
                 }
             </div>
         )
     })
 
-    const themePanel = themes.map( (g, key) => {
+    const themePanel = themes.map((g, key) => {
         return (
             <div className='Settings-panel' key={key + 6}>
                 <h3 className='Settings-title'>{g.title}</h3>
                 <div className='Themes-container'>
-                    {g.themes.map ( (v, key) => {
+                    {g.themes.map((v, key) => {
                         return (
-                            <NavigationTheme 
+                            <NavigationTheme
                                 v={v}
                                 key={key}
                             />
                         )
                     })}
                 </div>
-                
+
             </div>
         )
     })
@@ -111,7 +111,7 @@ const SettingsPanels = () => {
         </div>
     )
 
-    
+
 }
 
 export default SettingsPanels;
